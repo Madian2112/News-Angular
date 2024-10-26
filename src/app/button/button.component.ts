49e62876207fc3958b49b2ef8ffe7c504a4d310a
Madian2112 <madian.reyes212006@gmail.com>
2024-10-25 22:36:55 -0600

    initial commit

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Output, EventEmitter, Input, Inject  } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Output() botonClick = new EventEmitter<void>();
+  @Input() nombre? : String;
+
+  // Método que se llamará al hacer clic en el botón
+  onClick() {
+    this.botonClick.emit(); // Emitimos el evento al padre
+  }
+}
